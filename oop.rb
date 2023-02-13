13344ec23c94b17086e81caaa2425b32c2d6b949
Garrett Gregor <garrett.gregor@gmail.com>
2023-02-13 12:16:15 -0700

    Add oop homework and self-eval - Hobbit and Dragon not working

--- oop.rb
+++ oop.rb
@@ -1,24 +1,81 @@
-# Write a class called Unicorn
-# it should have a dynamic name attribute
-# it should have a color attribute, that is silver by default
-# it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
+# # # Write a class called Unicorn
+# # # it should have a dynamic name attribute
+# # # it should have a color attribute, that is silver by default
+# # # it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
+
+class Unicorn
+  attr_reader :name
+  def initialize(name)
+    @name = name
+    @color = "silver"
+  end
+
+  def say(saying)
+    puts "*~* #{saying} *~*"
+  end
+end
+
+uni1 = Unicorn.new("Silver Surfer")
+p uni1
+puts uni1.say("Look at me go")
 
 
 
-#  Write a class called Vampire
-#  it should have a dynamic name attribute
-#  it should have a pet attribute, that is a bat, by default BUT it could be dynamic if info is passed in initially
-#  it should have a thirsty attribute, that is true by default
-#  it should have a drink method. When called, the thirsty attribute changes to false
+# # #  Write a class called Vampire
+# # #  it should have a dynamic name attribute
+# # #  it should have a pet attribute, that is a bat, by default BUT it could be dynamic if info is passed in initially
+# # #  it should have a thirsty attribute, that is true by default
+# # #  it should have a drink method. When called, the thirsty attribute changes to false
+
+class Vampire
+  attr_reader :name, :pet, :thirsty
+  def initialize(name, pet = "bat", thirsty = true)
+    @name = name
+    @pet = pet
+    @thirsty = thirsty
+  end
+    
+  def drink
+    @thirsty = false
+  end
+end
+
+dracula = Vampire.new("Dracula", "cat")
+puts dracula.drink
 
 
-#  Write a Dragon class
-#  it should have a dynamic name attribute (string)
-#  it should have a dynamic rider attribute (string)
-#  it should have a dynamic color attribute (string)
-#  it should have a is_hungry attribute that is true by default
-#  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
+# #  Write a Dragon class
+# #  it should have a dynamic name attribute (string)
+# #  it should have a dynamic rider attribute (string)
+# #  it should have a dynamic color attribute (string)
+# #  it should have a is_hungry attribute that is true by default
+# #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
 
+class Dragon
+  attr_reader :is_hungry
+  def initialize(name, rider, color, is_hungry = 4)
+    @name = name
+    @rider = rider
+    @color = color
+    @is_hungry = is_hungry
+    if 
+      is_hungry <= 0; puts "The Dragon is no longer hungry"
+    else
+      puts "The dragon is hungry"
+    end
+  end
+    
+  def eat
+    eaten = @is_hungry -1
+    return eaten
+  end
+end
+
+dragon1 = Dragon.new("Norbert", "Garrett", "green")
+# I was expecting the code below to modify the dragon1, but it seems that the line below wants to default to 4 again 
+puts dragon1.eat
+p dragon1.is_hungry
+ 
 
 #  Write a Hobbit class
 #  it should have a dynamic name attribute (string)
@@ -28,3 +85,37 @@
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+
+class Hobbit
+  attr_reader :name, :disposition, :age, :is_adult, :is_old
+  def initialize(name, disposition, age = 0, has_ring = false, is_adult = false, is_old = false)
+    @name = name
+    @disposition = disposition
+    @age = age
+    @has_ring = has_ring
+      if name == "Frodo"
+        puts true
+      else
+        puts false
+      end
+    @is_adult = is_adult
+      if age >= 33
+        puts true
+      else
+        puts false
+      end
+    end
+
+  def celebrate_birthday
+    new_birthday = @age +1
+    return new_birthday
+  end
+end
+
+p hobbit1 = Hobbit.new("Frodo", "adventurous", 32)
+p hobbit1.celebrate_birthday
+# I still can't figure out why this doesn't store as a new variable
+puts hobbit1.is_adult
+
+
+
--- self-eval.md
+++ self-eval.md
@@ -1,5 +1,8 @@
 # Self-Evaluation
 
 - This final CFU encompassed everything you've learned in Mod 0, and maybe even required some learning outside of what you've explicitly been taught. How did it feel as you faced these challenges?
+  - This felt really frustrating a lot of the time. I used a lot of the tactics that I had implemented thus far, like taking a break and then coming back to the issue, but I just couldn't seem to figure out what was going on. I found it particularly frustrating when I was trying to research the answer online and feeling like I just wasn't phrasing it right, or that the answers were coming back in another language.
 - How is your workflow going, in terms of taking breaks, staying focused, etc.?
+  - In terms of taking breaks and staying focused, I feel like I'm doing a good job of going back and forth to the material and staying focused, I just wish I was able to figure out how to get less frustrated or do better with my researching when I ran into any roadblocks.
 - What did you learn from this exercise that you want to remember as you begin Mod 1?
+  - I learned that it is really helpful to ask for help and talk through things live with another developer, so I want to remember that it's ok to ask for help.
